668d312314c37b9df939b645b61ecdae37fbb1e1
Vicheanak <vicheanak@gmail.com>
2017-09-15 15:32:32 +0700

    mockup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import{AboutPage} from '../about/about';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
+
+    showConfirm() {
+        let confirm = this.alertCtrl.create({
+            title: 'ប្តូរយករង្វាន់នេះ?',
+            buttons: [
+                {
+                    text: 'ទេ',
+                    handler: () => {
+                        console.log('Disagree clicked');
+                    }
+                },
+                {
+                    text: 'ប្តូរ',
+                    handler: () => {
+                        console.log('Agree clicked');
+                        this.navCtrl.parent.select(1);
+                        //this.navCtrl.setPages([
+                        //    { page: AboutPage }
+                        //]);
+                    }
+                }
+            ]
+        });
+        confirm.present();
+    }
 
 }
